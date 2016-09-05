8b0e8bc74979a0295ae2c11e2f176fe02f266c9e
eliassa <esundbya@gmail.com>
2016-09-05 13:21:44 +0200

    c++ ov2 oppg1 DONE!

--- cpp/ov2/oppg1/main.cpp
+++ cpp/ov2/oppg1/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  int i =3;
+  int j =5;
+  int *p = &i;
+  int *q = &j;
+  
+  *p = 7;
+  *q += 4;
+  *q = *p+1;
+  p=q;
+  
+  cout << *p<< " "<<*q <<endl;
+  
+    cout << "variabel    verdi     adresse"<< endl;
+    cout << "i            "<<i << "         " << (long)&i<<endl;
+    cout << "j            "<<j << "         " <<(long) &j<<endl;
+    cout << "*p           "<<*p << "         " << (long)&*p<<endl;
+    cout << "*q           "<<*q << "         " << (long)&*q<<endl;
 }
