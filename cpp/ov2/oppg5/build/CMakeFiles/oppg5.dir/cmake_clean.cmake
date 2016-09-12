cf4552a7c4d0cfdaf5dc4ff1aec76a6aabc689a7
eliassa <esundbya@gmail.com>
2016-09-12 14:03:57 +0200

    ov2 cpp done! textfile added for oppg 2 and 3

---
+++ cpp/ov2/oppg5/build/CMakeFiles/oppg5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oppg5.dir/main.cpp.o"
+  "oppg5.pdb"
+  "oppg5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oppg5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/ov2/oppg6/build/CMakeFiles/oppg6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oppg6.dir/main.cpp.o"
+  "oppg6.pdb"
+  "oppg6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oppg6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
