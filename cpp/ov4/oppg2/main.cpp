38105523313fb3cb7a1adc49dd12455cd450bc4d
eliassa <esundbya@gmail.com>
2016-09-25 12:18:18 +0200

    some changes to cpp ov4

--- cpp/ov4/oppg2/main.cpp
+++ cpp/ov4/oppg2/main.cpp
@@ -8,40 +8,38 @@ public:
   Gtk::Entry etternavn;
   Gtk::Button button;
   Gtk::Label label;
+  Gtk::Label labelFirst;
+  Gtk::Label labelLast;
+  
 
   Window() {
     button.set_label("Click here");
+    labelFirst.set_text("first name");
+    labelLast.set_text("Last name");
 
+    vbox.pack_start(labelFirst);
     vbox.pack_start(fornavn);  //Add the widget entry to vbox
+    vbox.pack_start(labelLast);
     vbox.pack_start(etternavn);
     vbox.pack_start(button); //Add the widget button to vbox
     vbox.pack_start(label);  //Add the widget label to vbox
 
     add(vbox);  //Add vbox to window
     show_all(); //Show all widgets
+    button.set_sensitive(false);
     
-    fornavn.set_text("fornavn");
-    etternavn.set_text("etternavn");
 
     fornavn.signal_changed().connect([this]() {
-      label.set_text("names combined: " + fornavn.get_text()+ " "+ etternavn.get_text());
+      button.set_sensitive(fornavn.get_text().length()> 0 && etternavn.get_text().length()>0);
     });
     
     etternavn.signal_changed().connect([this](){
-      label.set_text("names combined: " + fornavn.get_text()+" " +etternavn.get_text());
+      button.set_sensitive(fornavn.get_text().length()> 0 && etternavn.get_text().length()>0);
     });
 
-    fornavn.signal_activate().connect([this]() {
-      label.set_text("Entry activated");
-      fornavn.set_text("");
-    });
-    
-    etternavn.signal_activate().connect([this]() {
-      label.set_text("Entry activated");
-    });
 
     button.signal_clicked().connect([this]() {
-      label.set_text("Button clicked");
+      label.set_text("Combined: " + fornavn.get_text() + " " + etternavn.get_text());
     });
   }
 };
